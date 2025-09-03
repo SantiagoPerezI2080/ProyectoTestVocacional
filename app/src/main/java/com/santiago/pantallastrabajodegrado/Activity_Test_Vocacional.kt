99f6d75901243392a82a32db7c9dda2ab70c1f36
Diana <diana.sanchez.o@uniautonoma.edu.co>
2025-09-02 23:25:10 -0500

    Detalles que olvide

--- app/src/main/java/com/santiago/pantallastrabajodegrado/Activity_Test_Vocacional.kt
+++ app/src/main/java/com/santiago/pantallastrabajodegrado/Activity_Test_Vocacional.kt
@@ -1,6 +1,8 @@
 package com.santiago.pantallastrabajodegrado
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,21 @@ class Activity_Test_Vocacional : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_test_vocacional)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Botón back
+        val btnBack = findViewById<ImageView>(R.id.btn_back)
+        btnBack.setOnClickListener {
+
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish() // Opcional: cierra esta Activity para no acumular en el stack
+        }
+
     }
 }
